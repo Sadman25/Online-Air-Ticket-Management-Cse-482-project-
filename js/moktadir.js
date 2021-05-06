2ce84eed4b3cd6bef388eaf88d65d041aa9bb2ef
moktadirkhan <moktadirkhan@gmail.com>
2021-05-07 02:57:28 +0600

    homepage #1 frontend commit

--- js/moktadir.js
+++ js/moktadir.js
@@ -103,4 +103,5 @@ var countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla"
 
 /*initiate the autocomplete function on the "myInput" element, and pass along the countries array as possible autocomplete values:*/
 autocomplete(document.getElementById("myInput"), countries);
+autocomplete(document.getElementById("myInput2"), countries);
 
