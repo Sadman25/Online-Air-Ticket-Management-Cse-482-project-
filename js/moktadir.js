94cc2552866b0ad0a16a902f5e65892035245d00
moktadirkhan <moktadirkhan@gmail.com>
2021-05-10 02:48:53 +0600

    homepage #1  frontend commit

--- js/moktadir.js
+++ js/moktadir.js
@@ -1,3 +1,43 @@
+$(document).ready(function(){
+
+  $("#one-way").click(function(){
+      $("#hideco").hide();
+      
+  });
+  
+  $("#roundtrip").click(function(){
+      $("#hideco").show();
+  });
+  
+  });
+
+
+  
+ // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
 
 function autocomplete(inp, arr) {
   /*the autocomplete function takes two arguments,
@@ -104,4 +144,6 @@ var countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla"
 /*initiate the autocomplete function on the "myInput" element, and pass along the countries array as possible autocomplete values:*/
 autocomplete(document.getElementById("myInput"), countries);
 autocomplete(document.getElementById("myInput2"), countries);
+
+
 
