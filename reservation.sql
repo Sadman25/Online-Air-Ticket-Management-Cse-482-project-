09f9951f4138086ed347fda31c2eae1f2c4a02ec
Md. Abdul Moktadir Khan <46750547+moktadirkhan@users.noreply.github.com>
2021-05-17 11:33:32 +0600

    Add files via upload

--- reservation.sql
+++ reservation.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 05, 2021 at 07:27 PM
+-- Generation Time: May 17, 2021 at 07:32 AM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 7.3.27
 
@@ -31,10 +31,20 @@ CREATE TABLE `airlines` (
   `id` int(11) NOT NULL,
   `airlines_name` varchar(50) NOT NULL,
   `airlines_model` varchar(50) NOT NULL,
-  `flight_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `flight_time` varchar(50) NOT NULL,
   `fare` varchar(10) NOT NULL,
-  `total_seat` int(11) NOT NULL
+  `total_seat` int(11) NOT NULL,
+  `destination` varchar(50) NOT NULL,
+  `depart` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `airlines`
+--
+
+INSERT INTO `airlines` (`id`, `airlines_name`, `airlines_model`, `flight_time`, `fare`, `total_seat`, `destination`, `depart`) VALUES
+(1, 'Etihad Airways', 'EA-1234', '9:00pm-10:00pm', '3200 tk', 52, 'SPD', 'DAC'),
+(2, 'Regent Airways', 'RA-1234', '1:00pm-2:00pm', '3500 tk', 36, 'DAC', 'SPD');
 
 -- --------------------------------------------------------
 
@@ -57,6 +67,22 @@ CREATE TABLE `reservation` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `temp_data`
+--
+
+CREATE TABLE `temp_data` (
+  `id` int(11) NOT NULL,
+  `trip_type` varchar(50) NOT NULL,
+  `CIdate` varchar(50) NOT NULL,
+  `COdate` varchar(50) NOT NULL,
+  `adult` int(11) NOT NULL,
+  `child` int(11) NOT NULL,
+  `air_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -67,6 +93,13 @@ CREATE TABLE `users` (
   `phone_number` varchar(20) NOT NULL,
   `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `phone_number`, `password`) VALUES
+(1, 'moktadirkhan', 'moktadirkhan@gmail.com', '01750671009', '1234567Jl');
 
 --
 -- Indexes for dumped tables
@@ -87,6 +120,12 @@ ALTER TABLE `reservation`
   ADD KEY `airlines_id` (`airlines_id`);
 
 --
+-- Indexes for table `temp_data`
+--
+ALTER TABLE `temp_data`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -100,7 +139,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `airlines`
 --
 ALTER TABLE `airlines`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `reservation`
@@ -109,10 +148,16 @@ ALTER TABLE `reservation`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `temp_data`
+--
+ALTER TABLE `temp_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
